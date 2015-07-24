3c041c9e9ba6bed37ad4814134b5ed221e80746d
ipsi16 <ipsita16@hotmail.com>
2015-07-24 14:17:53 +0530

    Initial code

---
+++ ScrabbleWordSuggestor.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+# include<map>
+class ScrabbleWordSuggestor {
+
+public:
+    map< string, vector<string> > sowpods;
+    map< int, vector<string> > wordsOfLength;
+    
+}
+int main()
+{
+	
+	map<>
+	getchar();
+	return 0;
+}
