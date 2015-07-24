e98715789bab174260be4b8df3aeeceebd9c03b0
ipsi16 <ipsita16@hotmail.com>
2015-07-24 14:38:31 +0530

    including relevant headers

--- ScrabbleWordSuggestor.cpp
+++ ScrabbleWordSuggestor.cpp
@@ -1,16 +1,21 @@
 # include<iostream>
 # include<map>
+# include<vector>
+
+using namespace std;
+
+
 class ScrabbleWordSuggestor {
 
 public:
     map< string, vector<string> > sowpods;
     map< int, vector<string> > wordsOfLength;
+private:
+	
     
-}
+};
 int main()
 {
-	
-	map<>
 	getchar();
 	return 0;
 }
