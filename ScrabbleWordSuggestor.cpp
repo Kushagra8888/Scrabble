c729c0af3650b99c87c1ba8e82a5c7b38bfd1e0e
mtbar131 <mtbar131@gmail.com>
2015-07-24 01:55:37 -0700

    generatePowerSetofRack function added

--- ScrabbleWordSuggestor.cpp
+++ ScrabbleWordSuggestor.cpp
@@ -5,7 +5,27 @@ class ScrabbleWordSuggestor {
 public:
     map< string, vector<string> > sowpods;
     map< int, vector<string> > wordsOfLength;
-    
+
+      void generatePowerSetOfRack( string rack, set<string>& power_set ) {
+        int set_size = rack.length();
+        unsigned int pow_set_size = pow(2, set_size);
+        int counter, j;
+        string subset;
+        int multiplicationFactor;
+
+        for(counter = 1; counter < pow_set_size; counter++) {
+            subset = "";
+
+
+            for(j = 0; j < set_size; j++) {
+                multiplicationFactor = pow (2, j);
+                if(counter & (1 * multiplicationFactor))
+                    subset += rack[j];
+            }
+            power_set.insert(subset);
+        }
+    }
+
 }
 int main()
 {
