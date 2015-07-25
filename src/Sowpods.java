0ffc08c4c33e14068ff8b6a2d7c6d923864c29a7
ipsi16 <ipsita16@hotmail.com>
2015-07-25 11:58:07 +0530

    sortWords function added to Sowpods

--- src/Sowpods.java
+++ src/Sowpods.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 /**
  * Created by test on 7/25/2015.
  */
-public class Sowpods {
+public class Sowpods 
+{
+	private String sortWord(String word)
+	{
+		char[] chars = word.toCharArray();
+		Arrays.sort(chars);
+		return new String(chars);
+	}
 }
