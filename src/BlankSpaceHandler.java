484af16894d2f86ca7092f0232b6fa101b5f24ff
unknown <mtbar131@gmail.com>
2015-07-25 11:40:42 +0530

    Blank Handler Class working complete. Testing Done

--- src/BlankSpaceHandler.java
+++ src/BlankSpaceHandler.java
@@ -8,10 +8,46 @@ import java.util.ArrayList;
 public class BlankSpaceHandler {
 
     ArrayList<String> spaceReplacedRack;
+    char blankChar;
 
-    public BlankSpaceHandler(){
+    public BlankSpaceHandler() {
         spaceReplacedRack = new ArrayList<String>();
+        blankChar = '_';
     }
 
-    public ArrayList<String> getBlankReplacedRacks(String )
+    public void generateRack(String rackWithSpaces) {
+
+
+        if (rackWithSpaces.contains(Character.toString(blankChar))) {
+
+            for (int i = 0; i < 26; i++) {
+                String newRack = "";
+                int j = 0;
+
+                while (rackWithSpaces.charAt(j) != '_') {
+                    newRack = newRack + rackWithSpaces.charAt(j);
+                    j++;
+                }
+
+                newRack = newRack + (char) ('a' + i) + rackWithSpaces.substring(j + 1);
+
+                generateRack(newRack);
+
+            }
+
+        } else {
+            spaceReplacedRack.add(rackWithSpaces);
+            return;
+        }
+    }
+
+    public ArrayList<String> getBlankReplacedRacks(String rackWithSpaces) {
+        generateRack(rackWithSpaces);
+        return spaceReplacedRack;
+    }
+
+    public static void main(String args[]) {
+        BlankSpaceHandler blankSpaceHandler = new BlankSpaceHandler();
+        System.out.println(blankSpaceHandler.getBlankReplacedRacks("ab_"));
+    }
 }
