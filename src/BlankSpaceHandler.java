e67b4be1f9d460d67596a4fccc111f5a2ab47726
unknown <mtbar131@gmail.com>
2015-07-25 11:11:16 +0530

    BlankSpaceHandler class definition added

---
+++ src/BlankSpaceHandler.java
@@ -0,0 +1,17 @@
+package Day6;
+
+import java.util.ArrayList;
+
+/**
+ * Created by test on 7/25/2015.
+ */
+public class BlankSpaceHandler {
+
+    ArrayList<String> spaceReplacedRack;
+
+    public BlankSpaceHandler(){
+        spaceReplacedRack = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getBlankReplacedRacks(String )
+}
