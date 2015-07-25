c16a56038911e80713f97e4ad9129d1e1c0538ed
mtbar131 <mtbar131@gmail.com>
2015-07-24 19:37:37 -0700

    functions arranged according to calling order and  code indented

--- ScrabbleWordSuggestor.cpp
+++ ScrabbleWordSuggestor.cpp
@@ -19,130 +19,25 @@ const int ALPHABET_SCORE[] = {1,3,3,2, 1,4,2,4, 1,8,5,1,3, 1,1,3,10, 1,1,1,1, 4,
 class ScrabbleWordSuggestor {
 
 public:
+  
   map< string, vector<string> > sowpods;
   map< int, vector<string> > scored_list;
   set < pair<string,int> > POWERSET_RACKS;
-    
-public:
   string RACK_STRING;
-  string constraint_spec;
   regex patternToCheck;
+  
+public: 
+  
   ScrabbleWordSuggestor(string rack, string constraint, ifstream& sowpodsFile) {
 
-    constraint_spec = constraint;    	
-    if(!(constraint_spec.empty())){	
-      patternToCheck = constraint_spec;      
+    if(!(constraint.empty())){	
+      patternToCheck = constraint;      
     }
+    
     generateSowpodsMap(sowpodsFile);
     generateScoredList(rack);
   }
 
-  // string convertToRegex(string constraint)
-  // {
-    
-  //   string regex = "";    
-  //   int i=0;
-  //   while (constraint[i] == ',' && i < constraint.length())
-  //     {
-  //   	i++;
-  //     }
-  //   int first_char = i++;
-  //   int last_char = 0;
-  //   while(i != constraint.length())
-  //     {
-  //   	if(constraint[i]!=',')
-  //   	  {
-  //   	    last_char = i;
-  //   	  }
-  //   	i++;
-  //     }
-  //   if(first_char)
-  //     regex += "(.*)";
-  //   for (int j = first_char; j <= last_char; j++)
-  //     {
-  //   	if(constraint[j] != ',')
-  //   	  {   
-  //   	    regex += constraint[j];
-  //   	  }
-  //   	else
-  //   	  {   
-  //   	    regex += '.';
-  //   	  }
-  //     }
-  //   if(last_char != constraint.length()-1)
-  //     regex += "(.*)";
-  //   cout<<regex<<endl;
-  //   return regex;
-    
-  // }
-
-  int getCharScore(char ch) {
-    return ALPHABET_SCORE[ ch - 'a' ];
-  }
-
-  void generatePowerSetOfRack( string rack, set<string>& power_set ) {
-    int set_size = rack.length();
-    unsigned int pow_set_size = pow(2, set_size);
-    int counter, j;
-    string subset;
-    int multiplicationFactor;
-
-    for(counter = 1; counter < pow_set_size; counter++) {
-      subset = "";
-
-
-      for(j = 0; j < set_size; j++) {
-	multiplicationFactor = pow (2, j);
-	if(counter & (1 * multiplicationFactor))
-	  subset += rack[j];
-      }
-      power_set.insert(subset);
-    }
-  }
-
-  void generateBlankReplacedPowerSet( set<string>& power_set, set< pair<string,int> >& power_set_score ) {
-    for ( string power_set_element : power_set ) {
-      replaceBlankTile( power_set_element, power_set_score );
-    }
-  }
-
-  void replaceBlankTile( string rack, set< std::pair<string,int> >& set_of_racks ) {
-    replaceBlankTile( rack, 0, set_of_racks );
-  }
-
-  void replaceBlankTile( string rack, int scoreDeductionValue, set< std::pair<string,int> >& set_of_racks ) {
-    int rackBlankTileIndex = rack.find(EMPTY_TILE);
-    if ( rackBlankTileIndex >= 0 ) {
-      for ( char ch = 'a'; ch <='z'; ch++ ) {
-	rack[rackBlankTileIndex] = ch;
-	int newScoreDectionValue = scoreDeductionValue + getCharScore(ch);
-	replaceBlankTile( rack, newScoreDectionValue, set_of_racks);
-      }
-    } else {
-      set_of_racks.insert( pair<string,int> ( getSortedString(rack), scoreDeductionValue) );
-    }
-  }
-
-  string getSortedString(string word) {
-    std::string sortedWord = word;
-    std::sort(sortedWord.begin(), sortedWord.end());
-    return sortedWord;
-  }
-
-  void insertInMap(string key, string value) {
-    map<string, vector<string> >::iterator it = sowpods.begin();
-
-    if( sowpods.count(key) ) {
-      it = sowpods.find( key );
-      (it->second).push_back(value);
-    }
-
-    else {
-      vector<string> newVector;
-      newVector.push_back(value);
-      sowpods.insert ( std::pair<string, vector<string> >( key, newVector) );
-    }
-  }
 
   void generateSowpodsMap(ifstream &file) {
     string word;
@@ -153,38 +48,14 @@ public:
     }
   }
 
-  vector<string> findInSowpodsMap(string key) {
-    vector<string> x;
-    if( sowpods.find(key) != sowpods.end() ) {
-      return sowpods.find( key )->second;
-    }
-    vector<string> a;
-    return a;
+  
+  string getSortedString(string word) {
+    std::string sortedWord = word;
+    std::sort(sortedWord.begin(), sortedWord.end());
+    return sortedWord;
   }
 
-  int computeScore(string str, int scoreCompensation) {
-    int score = 0;
-    for ( int i = 0; i < str.length(); i++ ) {
-      char ch = str[i];
-      if ( 'a' <= ch && ch <= 'z' ) {
-	score += ALPHABET_SCORE[ ch - 'a' ];
-      }
-    }
-    return score - scoreCompensation;;
-  }
-
-  void insertInScoredList(int score, string word) {
-    map<int, vector<string> >::iterator it = scored_list.begin();
-    if ( scored_list.find(score) != scored_list.end() ) {
-      it = scored_list.find( score );
-      (it->second).push_back(word);
-    } else {
-      vector<string> newVector;
-      newVector.push_back(word);
-      scored_list.insert ( std::pair<int, vector<string> >(score, newVector) );
-    }
-  }
-
+  
   void generateScoredList(string rack) {
     this->RACK_STRING = rack;
     set<string> powerSet;
@@ -203,13 +74,118 @@ public:
     }
   }
 
+
+  void generatePowerSetOfRack( string rack, set<string>& power_set ) {
+    int set_size = rack.length();
+    unsigned int pow_set_size = pow(2, set_size);
+
+    int counter, j;
+    string subset;
+    int multiplicationFactor;
+
+    for(counter = 1; counter < pow_set_size; counter++) {
+      subset = "";
+
+
+      for(j = 0; j < set_size; j++) {
+	multiplicationFactor = pow (2, j);
+	if(counter & (1 * multiplicationFactor))
+	  subset += rack[j];
+      }
+      power_set.insert(subset);
+    }
+  }
+
+
+  void generateBlankReplacedPowerSet( set<string>& power_set, set< pair<string,int> >& power_set_score ) {
+    for ( string power_set_element : power_set ) {
+      replaceBlankTile( power_set_element, power_set_score );
+    }
+  }
+
+
+  void replaceBlankTile( string rack, set< std::pair<string,int> >& set_of_racks ) {
+    replaceBlankTile( rack, 0, set_of_racks );
+  }
+
+
+  void replaceBlankTile( string rack, int scoreDeductionValue, set< std::pair<string,int> >& set_of_racks ) {
+    int rackBlankTileIndex = rack.find(EMPTY_TILE);
+    if ( rackBlankTileIndex >= 0 ) {
+      for ( char ch = 'a'; ch <='z'; ch++ ) {
+	rack[rackBlankTileIndex] = ch;
+	int newScoreDectionValue = scoreDeductionValue + getCharScore(ch);
+	replaceBlankTile( rack, newScoreDectionValue, set_of_racks);
+      }
+    } else {
+      set_of_racks.insert( pair<string,int> ( getSortedString(rack), scoreDeductionValue) );
+    }
+  }
+
+
+  int getCharScore(char ch) {
+    return ALPHABET_SCORE[ ch - 'a' ];
+  }
+
+
+  void insertInMap(string key, string value) {
+    map<string, vector<string> >::iterator it = sowpods.begin();
+
+    if( sowpods.count(key) ) {
+      it = sowpods.find( key );
+      (it->second).push_back(value);
+    }
+
+    else {
+      vector<string> newVector;
+      newVector.push_back(value);
+      sowpods.insert ( std::pair<string, vector<string> >( key, newVector) );
+    }
+  }
+
+
+  vector<string> findInSowpodsMap(string key) {
+    vector<string> x;
+    if( sowpods.find(key) != sowpods.end() ) {
+      return sowpods.find( key )->second;
+    }
+    vector<string> a;
+    return a;
+  }
+
+
+  int computeScore(string str, int scoreCompensation) {
+    int score = 0;
+    for ( int i = 0; i < str.length(); i++ ) {
+      char ch = str[i];
+      if ( 'a' <= ch && ch <= 'z' ) {
+	score += getCharScore(ch);
+      }
+    }
+    return score - scoreCompensation;;
+  }
+
+
+  void insertInScoredList(int score, string word) {
+    map<int, vector<string> >::iterator it = scored_list.begin();
+    if ( scored_list.find(score) != scored_list.end() ) {
+      it = scored_list.find( score );
+      (it->second).push_back(word);
+    } else {
+      vector<string> newVector;
+      newVector.push_back(word);
+      scored_list.insert ( std::pair<int, vector<string> >(score, newVector) );
+    }
+  }
+
+  
   void suggestWords(){
     	
     for ( map<int, vector<string> >::reverse_iterator r = scored_list.rbegin(); r != scored_list.rend(); ++r ) {
 
       cout << r->first << "\t\t" ;
       for ( string s : r->second ) {
-	if(!constraint_spec.empty() && regex_match(s,patternToCheck)){
+	if(!constraint.empty() && regex_match(s,patternToCheck)){
 	  cout << s << " " ;
 	}
       }
@@ -217,6 +193,7 @@ public:
     }
   }
 };
+
 
 /* we accept . for character and * for zero or more character in constratint */
 int main(int argc, char* argv[]) {
